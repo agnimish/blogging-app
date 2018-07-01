32cd93d74b5152f8e55f9c101b82c5bec18b250d
Nimish Agarwal <nimishagarwal.1997@gmail.com>
2018-07-01 22:58:58 +0530

    second commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # defining an action that returns desired html (string in this case)
+  def hello # Action
+      render html: "Hello, World!"
+  end
+
+  def goodbye
+    render html: "Goodbye, World!"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Setting the root route (<controllerName>#<actionName>).
+  # root 'application#hello'
+  root 'application#goodbye'
 end
